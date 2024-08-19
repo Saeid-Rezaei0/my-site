0f7aa0cf092f25d8fd21b9ea3abfb9b2f9fb06be
saeid <saeid.rezaei695@gmail.com>
2024-08-19 16:50:08 +0330

    project update in   github

--- components/ui/Socials.jsx
+++ components/ui/Socials.jsx
@@ -1,30 +1,34 @@
-import {FaGithub, FaInstagram, FaTelegram, } from "react-icons/fa"
+import { FaGithub, FaInstagram, FaTelegram } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import Link from "next/link";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
-function Socials({containerStyles, iconsStyles}) {
-    const socials = [
-        {icon: <FaGithub />,  path:"https://github.com/Saeid-Rezaei0", name: "گیت هاب"},
-        {icon: <FaInstagram />,  path:"https://www.instagram.com/saeid_89999", name: "اینستاگرام"},
-        {icon: <FaTelegram />,  path:"https://t.me/Saeidfrontend", name: "تلگرام"},
-        {icon: <AiOutlineMail />,  path:"https://saeid.rezaei695@gmail.com", name: "ایمیل"},
-    ]
+
+function Socials({ containerStyles, iconsStyles }) {
+  const socials = [
+    { icon: <FaGithub />, path: "https://github.com/Saeid-Rezaei0", name: "گیت هاب" },
+    { icon: <FaInstagram />, path: "https://www.instagram.com/saeid_89999", name: "اینستاگرام" },
+    { icon: <FaTelegram />, path: "https://t.me/Saeidfrontend", name: "تلگرام" },
+    { icon: <AiOutlineMail />, path: "mailto:saeid.rezaei695@gmail.com", name: "ایمیل" },
+  ];
+
   return (
     <div className={containerStyles}>
-        {socials.map(item => (
-          <TooltipProvider delayDuration={10}>
-            <Tooltip>
-                <TooltipTrigger>
-              <Link href={item.path} target="_blank" className={iconsStyles}>{item.icon}</Link>
-               </TooltipTrigger>
-                <TooltipContent className="mb-1 text-gray-100 background-custom z-50">
-                    <p >{item.name}</p>
-                </TooltipContent>
-            </Tooltip>
+      {socials.map((item) => (
+        <TooltipProvider key={item.path} delayDuration={10}>
+          <Tooltip>
+            <TooltipTrigger>
+              <Link href={item.path} target="_blank" className={iconsStyles}>
+                {item.icon}
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="mb-1 text-gray-100 background-custom z-50">
+              <p>{item.name}</p>
+            </TooltipContent>
+          </Tooltip>
         </TooltipProvider>
-        ))}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Socials
+export default Socials;
